e6624b6af3752e5ce8f315520d009368df5a8e55
yudhghjk <ludwig.stpsg@hotmail.com>
2024-05-13 20:29:00 +0700

    update project commit

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const binarySearchIterative = (arr, target) => {
-  let left = 0;
-  let right = arr.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (arr[mid] === target) {
-      return mid;
-    } else if (arr[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
-  }
-  return -1;
-};
+const isEven = (num) => num % 2 === 0;
