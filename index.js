6cc75562324d47ef68e967488f9b50f9bd2d85f4
yudhghjk <ludwig.stpsg@hotmail.com>
2024-05-15 06:14:12 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const isEven = (num) => num % 2 === 0;
+function findItinerary(tickets) {
+  const graph = new Map();
+  for (const [from, to] of tickets) {
+    if (!graph.has(from)) graph.set(from, []);
+    graph.get(from).push(to);
+  }
+  for (const destinations of graph.values()) {
+    destinations.sort();
+  }
+  const result = [];
+  dfs("JFK");
+  return result.reverse();
+  function dfs(from) {
+    const destinations = graph.get(from);
+    while (destinations && destinations.length) {
+      dfs(destinations.shift());
+    }
+    result.push(from);
+  }
+}
